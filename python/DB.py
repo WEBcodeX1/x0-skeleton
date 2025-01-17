ce29a3c1477bd84589fa148b8c95fa0c39387e06
Claus Prüfer <pruefer@webcodex.de>
2025-01-17 16:52:10 +0100

    Add global python skeleton

---
+++ python/DB.py
@@ -0,0 +1,61 @@
+import os
+import time
+import json
+
+from datetime import datetime
+
+import dbpool.pool
+
+
+DBName = 'your-db'
+DBUser = 'x0'
+DBHost = 'mypostgres'
+DBPass = os.environ['PSQL_x0_PWD']
+
+config = {
+    'db': {
+        'host': DBHost,
+        'name': DBName,
+        'user': DBUser,
+        'pass': DBPass,
+        'ssl': 'disable',
+        'connect_timeout': 4,
+        'connection_retry_sleep': 1,
+        'query_timeout': 5000,
+        'session_tmp_buffer': 128
+    },
+    'groups': {
+        'x0': {
+            'connection_count': 2,
+            'autoommit': True,
+        },
+        'clickit': {
+            'connection_count': 3,
+            'autoommit': True,
+        }
+    }
+}
+
+config_ac_disabled = {
+    'db': {
+        'host': DBHost,
+        'name': DBName,
+        'user': DBUser,
+        'pass': DBPass,
+        'ssl': 'disable',
+        'connect_timeout': 4,
+        'connection_retry_sleep': 1,
+        'query_timeout': 5000,
+        'session_tmp_buffer': 128
+    },
+    'groups': {
+        'x0noac': {
+            'connection_count': 2,
+            'autoommit': True,
+        },
+        'clickitnoac': {
+            'connection_count': 3,
+            'autoommit': False,
+        }
+    }
+}
