62477e1c41e3be5705616984521e848e2a5855dc
Claus Prüfer <pruefer@webcodex.de>
2025-01-25 14:43:50 +0100

    Fix docker id

--- docker/build-db.sh
+++ docker/build-db.sh
@@ -5,5 +5,5 @@ workdir="$(echo $PWD)"
 . ./scripts/get-env.sh
 
 cd ../../
-docker build -t your-app-db ${CMDLINE_ADD_HOST} --file ./x0-skeleton/docker/db.dockerfile .
+docker build -t your-db ${CMDLINE_ADD_HOST} --file ./x0-skeleton/docker/db.dockerfile .
 cd ${workdir}
