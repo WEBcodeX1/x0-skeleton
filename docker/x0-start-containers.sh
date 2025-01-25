90bc01a296480a8e615a7293fc501f8dfff4d4c3
Claus Prüfer <pruefer@webcodex.de>
2025-01-25 11:43:03 +0100

    Adapt container ids, add network creation

--- docker/x0-start-containers.sh
+++ docker/x0-start-containers.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
+# create network if not exists
+./x0-network.sh
+
 # stop containers, clean up
 ./x0-stop-containers.sh
 
 # start x0-db container
-docker run -d --name your-app-db --net x0-connect --ip 172.20.0.20 -p 5432:5432 your-app-db
+docker run -d --name x0-db --net x0-connect --ip 172.20.0.20 -p 5432:5432 x0-db
 
 # start your-app container
 docker run -d -\
--name your-app \
+-name x0-app \
 --add-host=mypostgres:172.20.0.20 \
 --net x0-connect \
 --ip 172.20.0.10 \
--p 80:80 -p 443:443 your-app
+-p 80:80 -p 443:443 x0-app
