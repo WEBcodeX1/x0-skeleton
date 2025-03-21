0234d1abd0fab73a6c9a08cd25a46939285bd3d7
Claus Prüfer <pruefer@webcodex.de>
2025-03-21 14:18:56 +0100

    Add first example code

---
+++ example/microesb/esbconfig.py
@@ -0,0 +1,11 @@
+config = {
+    'logging_enabled': False
+}
+
+import_classes = {
+    'service_implementation': [
+        'User',
+        'Domain',
+        'Host'
+    ]
+}
---
+++ example/x0-backend/getDomainsPulldown.py
@@ -0,0 +1,49 @@
+import sys
+import json
+
+import DB
+import dbpool.pool
+
+import POSTData
+
+config = {
+    'db': {
+        'host': 'mypostgres',
+        'name': 'hosting-example',
+        'user': 'postgres',
+        'pass': 'changeme',
+        'ssl': 'disable',
+        'connect_timeout': 30,
+        'connection_retry_sleep': 1,
+        'query_timeout': 30000,
+        'session_tmp_buffer': 128
+    },
+    'groups': {
+        'hosting': {
+            'connection_count': 3,
+            'autoommit': True,
+        }
+    }
+}
+
+dbpool.pool.Connection.init(config)
+
+
+def application(environ, start_response):
+
+    start_response('200 OK', [('Content-Type', 'application/json; charset=UTF-8')])
+
+    if environ['REQUEST_METHOD'].upper() == 'POST':
+
+        Result = []
+
+        sql = """SELECT name FROM sys_dns.dnsdomain ORDER BY name ASC"""
+        sql_params = {}
+        with dbpool.pool.Handler('hosting') as db:
+            for Record in db.query(sql, sql_params):
+                Row = {}
+                Row['Display'] = Record['name']
+                Row['Value'] = Record['name']
+                Result.append(Row)
+
+        yield bytes(json.dumps(Result), 'utf-8')
