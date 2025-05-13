9a9f6b2b7ccc1d0b66358d931e12a2f64b41d1f3
Claus Prüfer <pruefer@webcodex.de>
2025-05-13 18:57:16 +0200

    Automatic test(s) placed backwards

--- docker/build-test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-workdir="$(echo $PWD)"
-
-. ./scripts/get-env.sh
-
-cd ../../
-docker build -t your-app-test ${CMDLINE_ADD_HOST} --file ./x0-skeleton/docker/test.dockerfile .
-cd ${workdir}
